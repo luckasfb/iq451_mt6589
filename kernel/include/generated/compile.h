aa79eb075a7e1bcae5a7f6c206f02505505b368f
hyperion70 <bmaximov71@gmail.com>
2014-04-14 07:13:47 +0400

    first commit

---
+++ kernel/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 10 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#10 SMP PREEMPT Sun Apr 13 17:38:02 MSK 2014"
+#define LINUX_COMPILE_BY "hyperion"
+#define LINUX_COMPILE_HOST "root"
+#define LINUX_COMPILER "gcc version 4.6.3 (GCC) "
